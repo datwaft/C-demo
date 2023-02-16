a248073c72f3a60d569c025a3ebdeaa9101d37a8
datwaft <datwaftx@gmail.com>
2023-02-15 20:41:49 -0600

    ci(clang-tidy): test fix

--- src/main.c
+++ src/main.c
@@ -6,9 +6,9 @@ int main(int argc, char **argv) {
   for (int c = 'A'; c < 'A' + 26; c++) {
     // Colors
     // 31 = RED, 36 = CYAN
-    u_int32_t color = arc4random_uniform(36 - 31 + 1) + 31;
+    unsigned int color = arc4random_uniform(36 - 31 + 1) + 31;
     // Styles
-    u_int32_t style = arc4random_uniform(7 + 1);
+    unsigned int style = arc4random_uniform(7 + 1);
     printf("\x1b[%d;%dm"
            "%c"
            "\x1b[0m",
